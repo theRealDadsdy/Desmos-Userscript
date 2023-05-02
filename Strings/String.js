f47f7a4b4cc7bb82e62914518e1ba007b50ab147
Dadsdy <94847478+theRealDadsdy@users.noreply.github.com>
2023-05-02 14:24:21 -0400

    Update String.js

--- Strings/String.js
+++ Strings/String.js
@@ -1,1 +1,10 @@
+function stringToList(text){
+  return text.split('').map(function (char) {
+    return char.charCodeAt(0);
+  });
+}
 
+function listToString(nums){
+  const letters = nums.map(val => {return String.fromCharCode(val)});
+  return letters.join('');
+}
